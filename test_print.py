b82ead8b7ece99d106c572f85f4b8b6a0f51d335
MuhammadAndi93 <muhammadarfawzi@gmail.com>
2025-07-08 00:08:33 +0700

    buat file test_print.py

---
+++ test_print.py
@@ -0,0 +1,8 @@
+print("Hello My Name is Andi Fawzi" \
+"I am a Software Engineer" \
+    "I am a Python Developer" \
+        "I am a Full Stack Developer" \
+            "I am a DevOps Engineer" \
+                "I am a Cloud Engineer" \
+                    "I am a Data Scientist" \
+                        "I am a Machine Learning Engineer")
